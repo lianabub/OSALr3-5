638db1dd2d2f6fb477cfaf822e152b40ac0dbe30
liana <lianabub07@gmail.com>
2024-10-26 09:11:30 +0300

    Add files via upload

---
+++ lr3-5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream> 
+#include <math.h>
+using namespace std;
+int main() {
+ setlocale(LC_ALL, "RU");
+ int a, b, c;
+ bool n = false;
+ cout << "Введите числа";
+ cin >> a >> b >> c;
+ if (a % 2 == 1 || b % 2 == 0 || c % 2 == 0) {
+	 n = true;
+ }
+ else {
+	 cout << "No";
+ }
+ if (n) {
+	 if (a % 2 == 1 || b % 2 == 1 || c % 2 == 1) {
+		 cout << "Yes";
+	 }
+	 else {
+		 cout << "Yes";
+	 }
+ else { cout << "No"; }
+ }
+ return 0;
+
+
+
